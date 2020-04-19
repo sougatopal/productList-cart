60326937ccfbe88e60983534910ae0e8de139473
sougato Pal <sougatopal1@gmail.com>
2020-04-20 00:47:12 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
+import App from './components/App';import './css/styles.less';
 
-import App from './components/App';
-
-
-ReactDOM.render(
-    <BrowserRouter>
-        <Fragment>
-            <Provider >
-                <App />
-            </Provider>
-        </Fragment>
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+ReactDOM.render( 
+    <BrowserRouter> 
+    <Fragment> 
+    <App /> 
+    </Fragment> 
+    </BrowserRouter>, 
+    document.getElementById('root'));
