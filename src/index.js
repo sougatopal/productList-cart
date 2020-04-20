7fe9882df78eb64f57f97b238e498ea47a82d5d7
sougato Pal <sougatopal1@gmail.com>
2020-04-20 13:57:32 +0530

    added font awesome
    added font awesome

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter,Route,Switch } from 'react-router-dom';
-import App from './components/App';
-import './css/styles.less';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./components/App";
+import "./css/styles.less";
 
-ReactDOM.render( 
-    <BrowserRouter> 
-    <Fragment> 
-    <App /> 
-    </Fragment> 
-    </BrowserRouter>, 
-    document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Fragment>
+      <App />
+    </Fragment>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
